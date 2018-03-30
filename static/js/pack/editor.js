b8b910c20e609e8bb2b42be5ade436b9bf54ae33
Kirby Wu <tkirby@gmail.com>
2018-03-30 17:08:30 +0800

    tweak default color to choose

--- static/js/pack/editor.js
+++ static/js/pack/editor.js
@@ -34,22 +34,28 @@ c.setAttribute("max","7"),c.className="medium-editor-toolbar-input",b.appendChil
 
         // colors for picker
         var pickerColors = [
-          "#1abc9c",
-          "#2ecc71",
-          "#3498db",
-          "#9b59b6",
-          "#34495e",
-          "#16a085",
-          "#27ae60",
-          "#2980b9",
-          "#8e44ad",
-          "#2c3e50",
-          "#f1c40f",
-          "#e67e22",
-          "#e74c3c",
-          "#bdc3c7",
-          "#95a5a6",
-          "#f39c12"
+          "#212121",
+          "#8E8E8E",
+          "#C2C4C5",
+          "#ffffff",
+          "#F44336",
+          "#E91E63",
+          "#9C27B0",
+          "#673AB7",
+          "#3F51B5",
+          "#2196F3",
+          "#03A9F4",
+          "#00BCD4",
+          "#009688",
+          "#4CAF50",
+          "#8BC34A",
+          "#CDDC39",
+          "#FFEB3B",
+          "#FFC107",
+          "#FF9800",
+          "#FF5722",
+          "#795548",
+          "#607D8B"
         ];
 
         var picker = vanillaColorPicker(this.document.querySelector(".medium-editor-toolbar-active .editor-color-picker").parentNode);

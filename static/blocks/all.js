7c8e1051952ea5b5f67399e6e235f60229a89fa9
Kirby Wu <tkirby@gmail.com>
2018-04-13 17:07:36 +0800

    use image-progress-left and right to replace image-progress

--- static/blocks/all.js
+++ static/blocks/all.js
@@ -62,7 +62,14 @@ blocksManager.code.add('image-compare', function(module){
     }
   };
 });
-blocksManager.code.add('image-progress', function(module){
+blocksManager.code.add('image-progress-left', function(module){
+  return module.exports = {
+    wrap: function(){
+      return console.log('initialize blank block');
+    }
+  };
+});
+blocksManager.code.add('image-progress-right', function(module){
   return module.exports = {
     wrap: function(){
       return console.log('initialize blank block');
